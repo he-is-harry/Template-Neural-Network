05b584ab6c2cc7dd111ef14c38486ea4ff0fd240
he-is-harry <harryhe_@hotmail.com>
2022-07-13 12:54:37 -0400

    Changed Tester.java to avoid errors

--- FasterNeuralNetwork/Tester.java
+++ FasterNeuralNetwork/Tester.java
@@ -2,9 +2,8 @@ package FasterNeuralNetwork;
 
 public class Tester {
 	public static void main(String [] args) {
-		Equation e = new Equation("(x^5) - (x^3) + 2");
-		for(int i = -10; i < 10; i++) {
-			System.out.println(e.f(i));
-		}
+		/*
+		 * Here tests were made to the NeuralNetwork
+		 */
 	}
 }
